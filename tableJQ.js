d0bee430d6d8a5c15a6fc078b69b0561ee157e8e
sandhyasanju <p.sandhyarani20@gmail.com>
2015-11-17 19:05:39 +0530

    jquery

---
+++ tableJQ.js
@@ -0,0 +1,60 @@
+var $array = [
+  {
+    "s.no": "1",
+    "id": "4567",
+    "Name": "sandhya",
+    "gender": "f",
+    "age": "22",
+    "salary": "200000",
+    "status": "Active"
+  },
+  {
+    "s.no": "2",
+    "id": "4566",
+    "Name": "ajay",
+    "gender": "m",
+    "age": "24",
+    "salary": "100000",
+    "status": "InActive"
+  },
+  {
+    "s.no": "3",
+    "id": "4569",
+    "Name": "bhanu",
+    "gender": "f",
+    "age": "26",
+    "salary": "400000",
+    "status": "Active"
+  },
+  {
+    "s.no": "1",
+    "id": "4568",
+    "Name": "chandra",
+    "gender": "m",
+    "age": "25",
+    "salary": "250000",
+    "status": "InActive"
+  },
+  {
+    "s.no": "11",
+    "id": "4562",
+    "Name": "dev",
+    "gender": "f",
+    "age": "20",
+    "salary": "500000",
+    "status": "Active"
+  }
+];
+
+
+function create()
+{
+  var table = $("<table></table>");
+  var colnames = Object.keys(data[0]);
+  var thead = $("<thead></thead>");
+  for(i = 0 ; i < colnames.length ; i++ )
+  {
+
+  }
+
+}
