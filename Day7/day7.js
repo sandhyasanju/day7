ba14b295729fb00cdcc9ce7c46a5d372772d3e20
sandhyasanju <p.sandhyarani20@gmail.com>
2015-11-17 17:15:10 +0530

    day7 generic table sorting

--- Day7/day7.js
+++ Day7/day7.js
@@ -12,7 +12,21 @@ var data = [
   "age" : "23",
   "dob" : "12/06/94",
   "salary" : "30000"
-  }
+},
+{
+  "Name" : "niharika",
+  "Id" : "1459",
+  "age" : "21",
+  "dob" : "12/08/94",
+  "salary" : "40000"
+},
+{
+  "Name" : "sainiharika",
+  "Id" : "1659",
+  "age" : "21",
+  "dob" : "22/08/94",
+  "salary" : "10000"
+}
 ];
 function create()
 {
@@ -20,7 +34,7 @@ function create()
   var colnames = Object.keys(data[0]);
   for(var i = 0; i<colnames.length; i++)
   {
-  table += "<th>" + colnames[i] + "</th>" ;
+  table += "<th id = 'th'>" + colnames[i] + '<p onclick="sortingI(' + i + ')" class = "p">' + "&#x025B4" + '</p>' + '<p onclick = "sortingD(' + i + ')" class = "p">' + "&#x025BE" + "</p>" + "</th>" ;
   }
   table += "<tbody>";
   var l = colnames.length;
@@ -37,4 +51,42 @@ function create()
   tab.innerHTML=table;
   tab.id = "table2";
   tab.border = 1;
+}
+function sortingI(colid)
+{
+  var i, j, row1, row2, t;
+  var table = document.getElementById("table1");
+  for(i = 1 ; i < table.rows.length ; i++ )
+  {
+    for(j = 1 ; j < table.rows.length - 1 ; j++ )
+    {
+      row1 = table.rows[j].cells[colid].innerHTML;
+      row2 = table.rows[j+1].cells[colid].innerHTML;
+      if(row1 < row2)
+      {
+        t = table.rows[j].innerHTML;
+        table.rows[j].innerHTML = table.rows[j+1].innerHTML;
+        table.rows[j+1].innerHTML = t;
+      }
+    }
+  }
+}
+function sortingD(colid)
+{
+  var i, j, row1, row2, t;
+  var table = document.getElementById("table1");
+  for(i = 1 ; i < table.rows.length ; i++ )
+  {
+    for(j = 1 ; j < table.rows.length - 1 ; j++ )
+    {
+      row1 = table.rows[j].cells[colid].innerHTML;
+      row2 = table.rows[j+1].cells[colid].innerHTML;
+      if(row1 > row2)
+      {
+        t = table.rows[j].innerHTML;
+        table.rows[j].innerHTML = table.rows[j+1].innerHTML;
+        table.rows[j+1].innerHTML = t;
+      }
+    }
+  }
 }
